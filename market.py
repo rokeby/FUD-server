7f290fa384c4e9f0919e65480431236ce8b66be9
Agnes Cameron <agnesfcameron@gmail.com>
2020-10-12 13:16:29 +0100

    market seems to be working well

--- market.py
+++ market.py
@@ -32,7 +32,6 @@ class Bond:
 		self.bond_period = period
 
 	def est_return(self, time_remaining):
-		print('time remaining',  time_remaining, 'period', self.bond_period, 'fraction of time remaining', time_remaining/self.bond_period)
 		est = ((self.initial_price - self.price) + (time_remaining*self.bond_yield*self.initial_price)/self.bond_period)/self.price + 1
 		return est
 
@@ -71,19 +70,19 @@ class Agent:
 		#initially, reset each time
 		self.bid=None
 		self.ask=None
-		print('agent', self.name, 'has', len(self.bonds), 'bonds')
+		print('agent', self.name, 'has', len(self.bonds), 'bonds and $', self.funds)
 		if risk > self.risk_mean + 1.5*self.risk_std:
 			chat.update(self.name, "selling")
 			sellNum = len(self.bonds)
 			if sellNum > 0:
 				self.ask = Ask(self.bonds[0].est_return(time_remaining), self.bonds[0].price, sellNum, self.name)
-				print(self.name, 'asks', self.ask.est_return, self.ask.price, self.ask.num)
+				# print(self.name, 'asks', self.ask.est_return, self.ask.price, self.ask.num)
 
 		elif risk < self.risk_mean + 1.0*self.risk_std and risk > self.risk_mean - 1.0*self.risk_std:
 			chat.update(self.name, "buying")
 			desired_return=1.05
 			self.bid = Bid(desired_return, self.buy_limit(), self.name)
-			print(self.name, 'bid:', self.bid.desired_return, self.bid.vol)
+			# print(self.name, 'bid:', self.bid.desired_return, self.bid.vol)
 
 	def earnings():
 		print('calculate earnings')
@@ -150,11 +149,10 @@ def run_exchange(risk, time_remaining):
 		if len(market.bonds) > 0:
 			price = market.bonds[0].price
 			est_return = market.bonds[0].est_return(time_remaining)
-			print('est return is', est_return, 'desired return is', bid.desired_return)
+			# print('est return is', est_return, 'desired return is', bid.desired_return)
 
 			if est_return > bid.desired_return:
 				num_bonds = math.floor(bid.vol/float(price))
-				print(num_bonds, bid.vol, price)
 
 				#if not enough, sell all the bonds in the market to the agent
 				if num_bonds > len(market.bonds):
@@ -164,33 +162,41 @@ def run_exchange(risk, time_remaining):
 				bid_agent.bonds = bid_agent.bonds + market.bonds[:num_bonds]
 				bid_agent.funds = bid_agent.funds-price*num_bonds
 
+				#update the bid volume
+				bid.vol = bid.vol-price*num_bonds
+
 				#remove from the market
 				market.bonds = market.bonds[num_bonds:]
-				print('success, sold', num_bonds, 'bonds to ', bid_agent.name, len(market.bonds), 'remaining')
+				print('the market sold', num_bonds, 'bonds to ', bid_agent.name, len(market.bonds), 'remaining')
 
 		#then, if there are asks
 		if len(market.ask_list) > 0:
-			print('handling asks')
 
 			#make a copy so we can remove while we iterate
-			for ask in market.ask_list[:]:
+			for index, ask in enumerate(market.ask_list[:]):
 				ask_agent = next((agent for agent in agents if agent.name == ask.asker), None)
+				price = ask.price
 				if ask.est_return > bid.desired_return:
-					num_bonds = math.floor(bid.vol/ask.price)
+					num_bonds = math.floor(bid.vol/price)
 					if num_bonds > 0:
 						if num_bonds > ask.num:
 							num_bonds = ask.num
 
-						print('asker has', len(ask_agent.bonds), 'bidder has ', len(bid_agent.bonds))
+						# print('asker has', len(ask_agent.bonds), 'bidder has ', len(bid_agent.bonds))
 						bid_agent.bonds = bid_agent.bonds + ask_agent.bonds[:num_bonds]
 						bid_agent.funds = bid_agent.funds-price*num_bonds
 
 						ask_agent.bonds = ask_agent.bonds[num_bonds:]
-						ask_agent.funds = ask_agent.funds-price*num_bonds
+						ask_agent.funds = ask_agent.funds+price*num_bonds
 
-						ask.num = ask.num - num_bonds
-						print(ask_agent.name, 'sold', num_bonds, 'bonds to ', bid_agent.name)
-						print('now, asker has', len(ask_agent.bonds), 'bidder has ', len(bid_agent.bonds))
-
+						#update the ask
+						market.ask_list[index].num = ask.num - num_bonds
 						if ask.num == 0:
 							market.ask_list.remove(ask)
+
+						#update the bid
+						bid.vol = bid.vol-price*num_bonds
+
+						print(ask_agent.name, 'sold', num_bonds, 'bonds to ', bid_agent.name)
+						# print('now, asker has', len(ask_agent.bonds), 'bidder has ', len(bid_agent.bonds))
+
